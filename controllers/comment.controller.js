5d822882a0213f1c2fa34f687c18749d7bca2fd5
v4n3k <ivan71012@icloud.com>
2025-07-05 20:22:20 +0300

    feat(comment): Implement POST endpoint

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,40 +1,88 @@
+import { db } from '../db.js';
+
 class CommentController {
 	async getCommentsByVideoId(req, res) {
 		const { videoId } = req.params;
 
 		if (!videoId) {
-			res.status(400).json({ error: 'Missing required video id' });
+			return res.status(400).json({ error: 'Missing required video id' });
 		}
+
 
 		const commentsResult = await db.query(
-			`SELECT * FROM comments 
-				WHERE id = $1 
-				ORDER BY "createdAt"`,
+			`
+        SELECT
+            c.id AS "commentId",
+            c."videoId" AS "commentVideoId",
+            c.text AS "commentText",
+            c."createdAt" AS "commentCreatedAt",
+            c."parentCommentId" AS "commentParentId",
+
+            u_commenter.id AS "commenterUserId",
+            u_commenter.username AS "commenterUsername",
+            u_commenter."avatarUrl" AS "commenterAvatarUrl",
+
+            ch.id AS "channelId",
+            ch.name AS "channelName",
+            ch.description AS "channelDescription",
+            ch."avatarUrl" AS "channelAvatarUrl"
+        FROM
+            comments c
+        JOIN
+            users u_commenter ON c."userId" = u_commenter.id
+        LEFT JOIN
+            channels ch ON u_commenter.id = ch."userId"
+        WHERE
+            c."videoId" = $1
+        ORDER BY
+            c."createdAt" DESC;
+        `,
 			[videoId]
 		);
-		const comments = commentsResult.rows;
+
+		const comments = commentsResult.rows.map(row => {
+			const channel = row.channelId ? {
+				id: row.channelId,
+				name: row.channelName,
+				description: row.channelDescription,
+				avatarUrl: row.channelAvatarUrl,
+				author: {
+					id: row.commenterUserId,
+					name: row.commenterUsername,
+					avatarUrl: row.commenterAvatarUrl,
+				},
+			} : null;
+
+			return {
+				id: row.commentId,
+				videoId: row.commentVideoId,
+				text: row.commentText,
+				createdAt: row.commentCreatedAt,
+				parentCommentId: row.commentParentId,
+				channel: channel,
+			};
+		});
 
 		res.json(comments);
-	};
+	}
 
 	async createComment(req, res) {
-		const { userId, parentCommentId, text } = req.body;
+		const { videoId, text, parentCommentId } = req.body;
 
-		if (!userId || !parentCommentId || !text) {
-			res.status(400).json({ error: 'Missing required fields' });
+		if (!videoId || !text) {
+			return res.status(400).json({ error: 'Missing required fields' });
 		}
 
-		const commentResult = await db.query(
+		const comment = await db.query(
 			`INSERT INTO comments 
-				("userId", "parentCommentId", "text") 
-				VALUES ($1, $2, $3)
-				RETURNING *`,
-			[userId, parentCommentId, text]
+				("videoId", text, "userId", "parentCommentId") 
+				VALUES ($1, $2, $3, $4)
+			RETURNING *`,
+			[videoId, text, req.userId, parentCommentId]
 		);
-		const comment = commentResult.rows;
 
 		res.status(201).json(comment);
-	};
+	}
 }
 
 export default new CommentController();
