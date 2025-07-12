ede4a3a0fde22f638249b615da274ec1e24038ec
v4n3k <ivan71012@icloud.com>
2025-07-12 23:02:57 +0300

    feat(video): Add the rest of routes

--- routes/video.routes.js
+++ routes/video.routes.js
@@ -6,3 +6,14 @@ export const router = express.Router();
 
 router.get('/videos', handleError(VideoController.getRecommendedVideos));
 router.get('/videos/:id', handleError(VideoController.getVideoById));
+router.get('/channels/:channelId/videos', handleError(VideoController.getVideosByChannelId));
+router.get('/channels/:channelId/watchLater', handleError(VideoController.getWatchLaterVideosByChannelId));
+router.get('/channels/:channelId/history', handleError(VideoController.getHistoryVideosByChannelId));
+router.get('/channels/:channelId/liked', handleError(VideoController.getLikedVideosByChannelId));
+router.get('/channels/:channelId/subscribed', handleError(VideoController.getSubscribedVideosByChannelIds));
+router.post('/channels/:channelId/watchLater/:videoId', handleError(VideoController.addToWatchLater));
+router.delete('/channels/:channelId/watchLater/:videoId', handleError(VideoController.deleteFromWatchLater));
+router.post('/channels/:channelId/like/:videoId', handleError(VideoController.likeVideo));
+router.delete('/channels/:channelId/unlike/:videoId', handleError(VideoController.unlikeVideo));
+router.post('/channels/:channelId/dislike/:videoId', handleError(VideoController.dislikeVideo));
+router.delete('/channels/:channelId/undislike/:videoId', handleError(VideoController.undislikeVideo));
